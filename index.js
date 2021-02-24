0a516b23511db89d4cff4f51bbe63cd9fbb6afa6
Aditya Bisoi <adityabisoi1999@gmail.com>
2021-02-24 20:33:51 +0530

    download attachment with gmail api

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const fs = require('fs');
+global.atob = require("atob");
+global.Blob = require('node-blob');
+const save = require('save-file')
+
 const readline = require('readline');
 const {
     google
@@ -51,25 +55,25 @@ function getNewToken(oAuth2Client, callback) {
     const authUrl = oAuth2Client.generateAuthUrl({
         access_type: 'offline',
         scope: SCOPES,
-    });
+    })
     console.log('Authorize this app by visiting this url:', authUrl);
     const rl = readline.createInterface({
         input: process.stdin,
         output: process.stdout,
-    });
+    })
     rl.question('Enter the code from that page here: ', (code) => {
         rl.close();
         oAuth2Client.getToken(code, (err, token) => {
-            if (err) return console.error('Error retrieving access token', err);
-            oAuth2Client.setCredentials(token);
+            if (err) return console.error('Error retrieving access token', err)
+            oAuth2Client.setCredentials(token)
             // Store the token to disk for later program executions
             fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
                 if (err) return console.error(err);
-                console.log('Token stored to', TOKEN_PATH);
-            });
-            callback(oAuth2Client);
-        });
-    });
+                console.log('Token stored to', TOKEN_PATH)
+            })
+            callback(oAuth2Client)
+        })
+    })
 }
 
 /**
@@ -81,19 +85,63 @@ function listLabels(auth) {
     const gmail = google.gmail({
         version: 'v1',
         auth
-    });
-    gmail.users.labels.list({
+    })
+    gmail.users.messages.list({
         userId: 'me',
+        q: "is:unread"
     }, (err, res) => {
         if (err) return console.log('The API returned an error: ' + err);
-        const labels = res.data.labels;
-        if (labels.length) {
-            console.log('Labels:');
-            labels.forEach((label) => {
-                console.log(`- ${label.name}`);
-            });
-        } else {
-            console.log('No labels found.');
+        const msgs = res.data.messages
+        if (msgs.length) {
+            msgs.forEach((msg) => {
+                fetchEmail(msg.id)
+            })
         }
-    });
+    })
+
+    function fetchEmail(idData) {
+        gmail.users.messages.get({
+            userId: 'me',
+            id: idData
+        }, (err, res) => {
+            if (err) return console.log('The API returned an error: ' + err);
+            const payload = res.data.payload
+            for (let i = 0; i < payload.parts.length; i++) {
+                part = payload.parts[i]
+                const attachmentId = part.body.attachmentId
+                if (attachmentId) {
+                    downloadAttachment(attachmentId, idData, part.filename, part.mimeType)
+                }
+            }
+        })
+    }
+
+    function downloadAttachment(idnew, msgId, filename, mimeType) {
+        gmail.users.messages.attachments.get({
+            userId: 'me',
+            messageId: msgId,
+            id: idnew
+        }, (err, res) => {
+            if (err) return console.log('The API returned an error: ' + err)
+            const attachment = res.data.data
+            var base64 = (attachment).replace(/_/g, '/')
+            base64 = base64.replace(/-/g, '+')
+            var base64Fixed = fixBase64(base64)
+            var blob = new Blob([base64Fixed], {
+                type: mimeType
+            })
+            save(blob, 'images/' + filename)
+        })
+
+        function fixBase64(binaryData) {
+            var base64str = binaryData
+            var binary = atob(base64str.replace(/\s/g, ''))
+            var len = binary.length
+            var buffer = new ArrayBuffer(len)
+            var view = new Uint8Array(buffer)
+            for (var i = 0; i < len; i++)
+                view[i] = binary.charCodeAt(i)
+            return view
+        }
+    }
 }
